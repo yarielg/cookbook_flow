5be54cf8ec16d883bd6d42613e2340a9fd0f8d9b
yarielg <yariel.gordillo@gmail.com>
2021-12-19 16:39:46 -0500

    Adding bootstrap stylrd

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -26,6 +26,8 @@ class Enqueue{
 
     function memd_enqueue_frontend(){
 
+         wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css');
+
         $pageID = get_the_ID();
         $account_type = CBF_EMPTY_ACCOUNT;
 
