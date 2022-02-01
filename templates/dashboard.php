fbf132b9aa5da6523b6df96d57d4e72fd6a45200
yarielg <yariel.gordillo@gmail.com>
2022-02-01 10:00:10 -0500

    Fixing the header menu

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -22,40 +22,45 @@ if(is_user_logged_in()){
 
 <body <?php body_class(); ?>>
 <?php $current_user = wp_get_current_user(); ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light px-5 dashboard-header">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarTogglerDemo01">
-        <a class="navbar-brand " href="<?= site_url('welcome') ?>"><img src="https://cookbook.nextsitehosting.com/wp-content/uploads/2021/12/Logo.png"></a>
-        <ul class="navbar-nav mt-2 mt-lg-0 left-bar">
-            <a class="nav-link dropdown-toggle account-menu" href="#" id="create_action_dropmenu" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Create</a>
-            <div class="dropdown-menu" aria-labelledby="create_action_dropmenu">
-                <a class="dropdown-item" href="<?= site_url('welcome/?screen=add-recipe') ?>">Create recipe</a>
-                <?php if($user_data['premium']){?><a class="dropdown-item" href="<?= site_url('welcome/?screen=add-cookbook') ?>">Create cookbook</a><?php } ?>
-            </div>
-            <a class="nav-link" href="<?= site_url('welcome') ?>">Browse Recipe</a>
-            <a class="nav-link" href="<?= site_url('welcome') ?>">My Recipes</a>
-            <a class="nav-link" href="<?= site_url('welcome') ?>">My Cookbooks</a>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle account-menu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img src="<?php echo CBF_PLUGIN_URL . 'assets/images/user.png' ?>" alt=""> <?php echo $current_user->user_firstname ?>
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <?php if($user_data['account_type'] == CBF_OWNER_ACCOUNT){ ?>
-                        <a class="dropdown-item" href="<?= site_url('welcome/?screen=collaborators') ?>">Collaborators</a>
-                        <a class="dropdown-item" href="<?= site_url('register/your-membership/') ?>">Your Account</a>
-                    <?php } ?>
-                    <a class="dropdown-item" href="#"></a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="<?= site_url('/wp-login.php?action=logout') ?>">Sign out</a>
-                </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
+        <a class="navbar-brand " href="<?= site_url('welcome') ?>"><img style="width: 150px" src="https://cookbook.nextsitehosting.com/wp-content/uploads/2021/12/Logo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            </li>
-        </ul>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
 
-    </div>
-</nav>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <ul class="navbar-nav mt-2 mt-lg-0 left-bar">
+                    <a class="nav-link dropdown-toggle account-menu" href="#" id="create_action_dropmenu" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Create</a>
+                    <div class="dropdown-menu" aria-labelledby="create_action_dropmenu">
+                        <a class="dropdown-item" href="<?= site_url('welcome/?screen=add-recipe') ?>">Create recipe</a>
+			            <?php if($user_data['premium']){?><a class="dropdown-item" href="<?= site_url('welcome/?screen=add-cookbook') ?>">Create cookbook</a><?php } ?>
+                    </div>
+                    <a class="nav-link" href="<?= site_url('welcome') ?>">Browse Recipe</a>
+                    <a class="nav-link" href="<?= site_url('welcome') ?>">My Recipes</a>
+                    <a class="nav-link" href="<?= site_url('welcome') ?>">My Cookbooks</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle account-menu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="<?php echo CBF_PLUGIN_URL . 'assets/images/user.png' ?>" alt=""> <?php echo $current_user->user_firstname ?>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				            <?php if($user_data['account_type'] == CBF_OWNER_ACCOUNT){ ?>
+                                <a class="dropdown-item" href="<?= site_url('welcome/?screen=collaborators') ?>">Collaborators</a>
+                                <a class="dropdown-item" href="<?= site_url('register/your-membership/') ?>">Your Account</a>
+				            <?php } ?>
+                            <a class="dropdown-item" href="#"></a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="<?= site_url('/wp-login.php?action=logout') ?>">Sign out</a>
+                        </div>
+
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </nav>
 <?php }else{
 
     if($post_slug !== 'register'){
