8d13460d0cca95533b815154084993c15c2c0149
yarielg <yariel.gordillo@gmail.com>
2022-04-01 08:35:23 -0400

    Adding fixes 4/1

--- templates/featured-recipes.php
+++ templates/featured-recipes.php
@@ -1,16 +1,4 @@
-<style>
-	.recipe_featured_container .featured_item{
-		background-size: cover;
-		height: 200px
-	}
-	.recipe_featured_container .flag img{
-		width: <?php echo $a['flag_width'] ?>px;
-		height: <?php echo $a['flag_height'] ?>px;
-		position: absolute;
-		bottom: 20px;
-		left: 30px;
-	}
-</style>
+
 <div class="recipe_featured_container container">
 	<div class="row">
 		<?php
@@ -27,10 +15,9 @@
 		?>
 		<div class="col-md-<?php echo $cols ?> mb-5">
 			<a href="<?php echo get_permalink( get_the_ID() ) ?>">
-				<div class="featured_item" style="background-image: url(<?php echo $featured ?>);">
-					<!--<h5><?php /*echo get_the_title() */?></h5>-->
+				<div class="featured_item" style="background-image: url(<?php echo $featured ?>);background-size: cover;height: 200px">
 					<div class="flag">
-						<img src="<?php echo !empty($flag) ? CBF_PLUGIN_URL . '/assets/images/flags/'. $flag . '.png' : '' ?>" alt="">
+						<img style="width:<?php echo $a['flag_width'] ?>px; height:  <?php echo $a['flag_height'] ?>px;position: absolute;bottom: 20px;left: 30px;" src="<?php echo !empty($flag) ? CBF_PLUGIN_URL . '/assets/images/flags/'. $flag . '.png' : '' ?>" alt="">
 					</div>
 				</div>
 			</a>
